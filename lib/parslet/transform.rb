e0c53eb2ee065eabda5cfbce5d88b4eedc7d7b5e
Doug Puchalski <doug316@gmail.com>
2013-03-29 00:51:27 -0700

    Integrate Context into Transform.
    Keeping this in a separate file is problematic due to dependency resolution in RubyMotion as an inherent circular dependency between the scopes.

--- lib/parslet/transform.rb
+++ lib/parslet/transform.rb
@@ -1,5 +1,6 @@
 
 require 'parslet/pattern'
+require 'blankslate'
 
 # Transforms an expression tree into something else. The transformation
 # performs a depth-first, post-order traversal of the expression tree. During
@@ -233,4 +234,36 @@ class Parslet::Transform
   def recurse_array(ary, ctx) 
     ary.map { |elt| apply(elt, ctx) }
   end
+
+  # Provides a context for tree transformations to run in. The context allows
+  # accessing each of the bindings in the bindings hash as local method.
+  #
+  # Example:
+  #
+  #   ctx = Context.new(:a => :b)
+  #   ctx.instance_eval do
+  #     a # => :b
+  #   end
+  #
+  class Context < BlankSlate
+    reveal :methods
+    reveal :respond_to?
+    reveal :inspect
+    reveal :to_s
+    reveal :instance_variable_set
+
+    def meta_def(name, &body)
+      metaclass = class <<self; self; end
+
+      metaclass.send(:define_method, name, &body)
+    end
+
+    def initialize(bindings)
+      bindings.each do |key, value|
+        meta_def(key.to_sym) { value }
+        instance_variable_set("@#{key}", value)
+      end
+    end
+  end
+
 end
